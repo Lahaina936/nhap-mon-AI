02a7869c9e74d369d7c268d85549dddd6e21db13
tranquang <flash291101@gmail.com>
2022-12-16 14:07:54 +0700

    first

---
+++ test/src/Factory/StrategyFactory.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Factory;
+
+import Strategy.MazeGenerationStrategy.BacktrackingStrategy;
+import Strategy.MazeGenerationStrategy.MazeGenerationStrategy;
+import Strategy.HeuristicStrategy.DiagonalStrategy;
+import Strategy.HeuristicStrategy.EuclideanStrategy;
+import Strategy.HeuristicStrategy.HeuristicStrategy;
+import Strategy.HeuristicStrategy.ManhattanStrategy;
+import Strategy.HeuristicStrategy.PythagorasStrategy;
+import Strategy.MazeGenerationStrategy.KruskalStrategy;
+import Strategy.MazeGenerationStrategy.PrimStrategy;
+import Strategy.MazeGenerationStrategy.SidewinderStrategy;
+import Strategy.PathfindingStrategy.AStarStrategy;
+import Strategy.PathfindingStrategy.DijkstraStrategy;
+import Strategy.PathfindingStrategy.PathfindingStrategy;
+import Strategy.PathfindingStrategy.WavePropagationStrategy;
+
+/**
+ * Factory that returns a pathfinding strategy
+ * @author frank
+ */
+public class StrategyFactory
+{
+    // Pathfinding Factory
+    public static PathfindingStrategy getPathfindingStrategy(PathfindingStrategy.Algorithms algorithmStrategy, HeuristicStrategy heuristicStrategy)
+    {
+        switch(algorithmStrategy)
+        {
+            case Dijkstra:
+                return new DijkstraStrategy();
+            case AStar:
+                return new AStarStrategy(false, heuristicStrategy);
+            case AStarOptimal:
+                return new AStarStrategy(true, heuristicStrategy);
+            case WavePropagation:
+                return new WavePropagationStrategy();
+            default:
+                throw new IllegalArgumentException("Pathfinding algorithm not found!");
+        }
+    }
+    
+    // Heuristic for A* Factory
+    public static HeuristicStrategy getHeuristicStrategy(AStarStrategy.Heuristic strategy)
+    {
+        switch(strategy)
+        {
+            case Pythagoras:
+                return new PythagorasStrategy();
+            case Manhattan:
+                return new ManhattanStrategy();
+            case Diagonal:
+                return new DiagonalStrategy();
+            case Eudclidean:
+                return new EuclideanStrategy();
+            default:
+                throw new IllegalArgumentException("Heuristic strategy not found!");
+        }
+    }
+    
+    // Maze generation Factory
+    public static MazeGenerationStrategy getMazeGenStrategy(MazeGenerationStrategy.MazeGen strategy)
+    {
+        switch(strategy)
+        {
+            case Backtracker:
+               return new BacktrackingStrategy();
+            case Kruskal:
+                return new KruskalStrategy();
+            case Prim:
+                return new PrimStrategy();
+            case Sidewinder:
+                return new SidewinderStrategy();
+            default:
+                throw new IllegalArgumentException("Maze generation algorithm not found!");
+        }
+    }
+}
---
+++ test/src/Strategy/PathfindingStrategy/WavePropagationStrategy.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Strategy.PathfindingStrategy;
+
+import Model.Grid;
+import Model.Tile;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Wave propagation pathfinding strategy. 
+ * This pathfinding strategy does not support a weighted search!
+ * @author frank
+ */
+public class WavePropagationStrategy extends PathfindingStrategy
+{
+    private final int DEFAULT_DISTANCE = 1;
+    
+    public WavePropagationStrategy()
+    {
+        super();
+    }
+    
+    @Override
+    protected int runPathfinder(Grid model, List<Tile> path)
+    {
+        HashMap<Tile, Integer> tileData = new HashMap<>();
+        this.executeWavePropagation(model, tileData);
+        
+        this.buildPath(path, model, tileData);
+        int cost = tileData.get(model.getTarget()) - 1;
+        
+        this.statistics.setPathFound(true, cost);
+        this.painter.drawPath(path, model);
+        
+        return cost;
+    }
+    
+    /**
+     * Executes a wave propagation algorithm to find the path between root and target
+     * @param model
+     * @param tileData 
+     */
+    private void executeWavePropagation(Grid model, HashMap<Tile, Integer> tileData)
+    {
+        // Keeps track of visited tiles
+        Set<Tile> visited = new HashSet<>();
+        // List of cells to be processed in the while iteration
+        List<Tile> toProcess = new LinkedList<>();
+        // List of unprocessed cells (collected neighbors of toProcess)
+        List<Tile> unprocessed = new LinkedList<>();
+        // Current distance from target node to current iterations
+        int iteration = 0;
+        
+        unprocessed.add(model.getRoot());
+        visited.add(model.getRoot());
+        tileData.put(model.getRoot(), this.DEFAULT_DISTANCE);
+        
+        while(!unprocessed.isEmpty())
+        {
+            iteration++;
+            // shifts all elements from unprocced to toProcess 
+            toProcess.addAll(unprocessed);
+            unprocessed.clear();
+            
+            // Each iterations processed all neighbors from toProcess
+            for(Tile tile : toProcess)
+            {
+                List<Tile> neighbors = model.getTileNeighbors(tile);
+                
+                for(Tile elem : neighbors)
+                {
+                    if(elem == null) continue;
+                    if(elem.isWall()) continue;
+                    if(visited.contains(elem)) continue;
+                    
+                    unprocessed.add(elem);
+                    visited.add(elem);
+                    this.statistics.incrementVisited();
+                    tileData.put(elem, this.DEFAULT_DISTANCE + iteration);
+                    
+                    this.painter.drawTile(elem, model.getTarget(), model.getRoot(), Tile.Type.HIGHLIGHT, 2);
+                    this.painter.drawTile(elem, model.getTarget(), model.getRoot(), Tile.Type.VISITED, 4);
+                }
+            }
+        }
+    }
+    
+    /**
+     * Builds path based on hashmap distances collected from algorithm
+     * @param path [out] path list to store the final path
+     * @param model grid
+     * @param data hashmap mapping tiles to their distances
+     */
+    private void buildPath(List<Tile> path, Grid model, HashMap<Tile, Integer> data)
+    {
+        Tile currentTile = model.getTarget();
+         
+        do{
+            path.add(currentTile);
+            Tile lowestCost = null;
+            
+            for(Tile tile : model.getTileNeighbors(currentTile))
+            {
+                if(tile == null) continue;
+                if(tile.isWall()) continue;
+                
+                if(lowestCost == null)
+                {
+                    lowestCost = tile;
+                    continue;
+                }
+                
+                if(data.get(tile) < data.get(lowestCost))
+                    lowestCost = tile;
+            }
+            currentTile = lowestCost;
+        } while (currentTile != model.getRoot());
+        
+        Collections.reverse(path);
+    }
+}
